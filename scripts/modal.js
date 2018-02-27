87f3ff27803aac6d2bf2730dcfa79c8f52b7fc59
HuMont <hugomontagne@gmail.com>
2018-02-27 11:18:29 +0000

    modal script further changes

--- scripts/modal.js
+++ scripts/modal.js
@@ -10,17 +10,22 @@ function openModal(linkName) {
       document.getElementById('event__modal').style.display = "block";
       document.getElementById('accomadation__modal').style.display = "none";
       document.getElementById('location__modal').style.display = "none";
+      location.href = "#event__modal";
     break;
     case "accomadation__link":
       document.getElementById('accomadation__modal').style.display = "block";
       document.getElementById('event__modal').style.display = "none";
       document.getElementById('location__modal').style.display = "none";
+      location.href = "#accomadation__modal";
     break;
     case "location__link":
       document.getElementById('location__modal').style.display = "block";
       document.getElementById('accomadation__modal').style.display = "none";
       document.getElementById('event__modal').style.display = "none";
+      location.href = "#location__modal";
     break;
     default:
   }
+  
+  
 }
